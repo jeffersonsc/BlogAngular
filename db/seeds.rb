61e2f86a1399627548087e1cc7b27765757ff5b6
Jefferson Cunha <jeffersondasilva14@gmail.com>
2015-12-14 17:23:40 -0200

    Criado estrutura de posts
    Signed-off-by: Jefferson Cunha <jeffersondasilva14@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create([{title: 'Primeiro post', link: 'default', upvotes: 0}, {title: 'Post de tete', link: 'default', upvotes: 0}])
